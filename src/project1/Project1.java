4ff536313f9e86e1ca4b14c84609f6d6a625c341
janghj12000 <janghj12000@hotmail.com>
2015-03-30 22:12:09 -0700

    wav file reader
    This read wave file.
    Then check max/min sample, number of sample and draw wave diagram

---
+++ src/project1/Project1.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package project1;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author hja23
+ */
+public class Project1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        JFrame aa = new soudnframe();
+        aa.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        aa.setVisible(true);
+        
+        
+    }
+    
+}
